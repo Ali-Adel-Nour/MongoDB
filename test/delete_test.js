d0178d5d2b29ca0f3123b4e069eb8973642ffc0f
Ali-Adel-Nour <aliiiadel841@gmail.com>
2023-09-09 18:09:08 +0300

    Delete

--- test/delete_test.js
+++ test/delete_test.js
@@ -1,36 +1,38 @@
 const assert = require('assert');
-
 const User = require('../src/user');
 
 describe('Deleting a user', () => {
   let ali;
 
   beforeEach(async () => {
-      ali = new User({ name: 'Ali' });
-      await ali.save();
+    ali = new User({ name: 'Ali' });
+    await ali.save();
   });
 
   it('model instance remove', async () => {
-      await ali.deleteOne();
-      const user = await User.findOne({ name: 'Ali' });
-      assert(user === null);
+    await ali.deleteOne(); // Use remove() to delete the instance
+    const user = await User.findOne({ name: 'Ali' });
+    assert(user === null);
+  });
+
+  it('class method remove', async () => {
+    // Remove a user by finding and removing using class method
+    await User.deleteOne({ name: 'Ali' });
+    const user = await User.findOne({ name: 'Ali' });
+    assert(user === null);
+  });
+
+  it('class method findOneAndRemove', async () => {
+    // Find and remove a user using class method findOneAndRemove()
+    await User.findOneAndRemove({ name: 'Ali' });
+    const user = await User.findOne({ name: 'Ali' });
+    assert(user === null);
+  });
+
+  it('class method findByIdAndRemove', async () => {
+    // Find and remove a user by ID using class method findByIdAndRemove()
+    await User.findByIdAndRemove(ali._id);
+    const user = await User.findOne({ name: 'Ali' });
+    assert(user === null);
   });
 });
-
-
-    it('class method remove', () => {
-
-      //Remove of bunch of records with some given criteraia
-
-
-
-
-
-    });
-
-    it('class method findAndRemove', () => {
-        //User
-    });
-
-    it('class method findByIdAndRemove', () => {});
-
